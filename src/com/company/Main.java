8165c56b750fd41a2a1152bb6de51bd4b44a4ed5
ПрограммистДима <BonmenZ21@yandex.ru>
2022-04-10 12:34:48 +0300

    Первое изменения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,29 @@
 package com.company;
-import java.io.*;
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-        //Ветка Димы
-        InputStream inputStream = System.in;
-        Reader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String sAge = bufferedReader.readLine();
-        int a = Integer.parseInt(sAge);
-
-        int b = a*2;
-
-        System.out.println(b);
+        public static void main(String[] args) {
+            double num1;
+            double num2;
+            double ans;
+            char operation;
+            Scanner reader = new Scanner(System.in);
+            System.out.print("Вводи");
+            num1 = reader.nextDouble();
+            num2 = reader.nextDouble();
+            System.out.print("Вводи операцию (+, -, *, /): ");
+            operation = reader.next().charAt(0);
+            switch(operation) {
+                case '+': ans = num1 + num2;
+                    break;
+                case '-': ans = num1 - num2;
+                    break;
+                case '*': ans = num1 * num2;
+                    break;
+                case '/': ans = num1 / num2;
+                    break;
+                default:  System.out.print("тварь");
+                    return;
+            }
+            System.out.print(num1 + " " + operation + " " + num2 + " = " + ans);
+        }
     }
-}
