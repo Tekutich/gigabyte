74138dd28ae0d9fc1068fccd43ffe648f989bd1d
ПрограммистДима <BonmenZ21@yandex.ru>
2022-04-10 12:14:57 +0300

    Первое изменения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
-
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //Ветка Димы
-        System.out.println("aboba");
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String sAge = bufferedReader.readLine();
+        int nAge = Integer.parseInt(sAge);
     }
 }
