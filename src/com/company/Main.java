70d9cc442d00bd1b22788a21165a3d88dcb86182
RuslanGordin <101972460+ruslanGordin@users.noreply.github.com>
2022-04-10 16:17:36 +0700

    Ruslan1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         //Ruslan
         System.out.println("привет");
-        System.out.println("b");
+        System.out.println(b);
     }
 }
