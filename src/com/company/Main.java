90a42696f07b990543598a00f87ce78a77109e00
AlexNort <dekart11112005@gmail.com>
2022-04-10 12:05:17 +0300

    БУНД (2 изменение)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         //Ветка Артёма
         System.out.println("БУНТ!!!");
+        System.out.println("За мной кто то наблюдает");
     }
 }
