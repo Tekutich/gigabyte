9029d0674cafa80c0fdad6cd34de60f9a7a015bb
RuslanGordin <101972460+ruslanGordin@users.noreply.github.com>
2022-04-24 16:39:56 +0700

    добавление корня квадрата и степени

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,9 +32,6 @@ public class Main {
 
 
          public static void main(String[] args) {
-            System.out.println(Math.pow(4,2));
-            System.out.println(Math.sqrt(4));
-            System.out.println(Math.pow(4,4));
             double num1;
             double num2;
             double ans;
