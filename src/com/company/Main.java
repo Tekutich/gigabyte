bdf3454af09185bd20cc296993c90a037662ff8b
RuslanGordin <101972460+ruslanGordin@users.noreply.github.com>
2022-04-24 16:37:19 +0700

    добавление корня квадрата и степени.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,25 +17,48 @@ public class Main {
             return a/b;
         }
 
-        public static void main(String[] args) {
+        protected static double square(double a){
+            return Math.pow(a,2);
+        }
+
+        protected static double root(double a){
+            return Math.sqrt(a);
+        }
+
+        protected static double pow(double a, double b){
+            return Math.pow(a, b);
+        }
+
+
+
+         public static void main(String[] args) {
+            System.out.println(Math.pow(4,2));
+            System.out.println(Math.sqrt(4));
+            System.out.println(Math.pow(4,4));
             double num1;
             double num2;
             double ans;
-            char operation;
+            String operation;
             Scanner reader = new Scanner(System.in);
             System.out.print("Вводи");
             num1 = reader.nextDouble();
             num2 = reader.nextDouble();
             System.out.print("Вводи операцию (+, -, *, /): ");
-            operation = reader.next().charAt(0);
+            operation = reader.next();
             switch(operation) {
-                case '+': ans = sum(num1,num2);
+                case "+": ans = sum(num1,num2);
                     break;
-                case '-': ans = minus(num1,num2);
+                case "-": ans = minus(num1,num2);
                     break;
-                case '*': ans = multiply(num1,num2);
+                case "*": ans = multiply(num1,num2);
                     break;
-                case '/': ans = division(num1,num2);
+                case "/": ans = division(num1,num2);
+                    break;
+                case "square": ans = square(num1);
+                    break;
+                case "root": ans = root(num1);
+                    break;
+                case "pow": ans = pow(num1,num2);
                     break;
                 default:  System.out.print("тварь");
                     return;
